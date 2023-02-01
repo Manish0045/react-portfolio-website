08b1cfb4fc581d0f766117cfd5fb723a008f2938
Manish <101914715+Manish0045@users.noreply.github.com>
2023-02-01 11:12:37 +0530

    some changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,14 +3,30 @@ import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/MS2.jpg";
 import HeaderSocials from "./HeaderSocials";
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 const Header = () => {
+  const [text] = useTypewriter({
+    words: ['Coder', 'UI/UX developer', 'Web developer', 'Full stack developer'],
+    loop: 0,
+    typeSpeed: 20,
+    deleteSpeed: 10,
+    delaySpeed: 2000
+  });
   return (
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Manish Sahani</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h3 className="text-light">
+          <span>
+            {text}
+          </span>
+          <Cursor
+            cursorColor="red"
+            cursorStyle="_"
+          />
+        </h3>
         <CTA />
         <HeaderSocials />
 
